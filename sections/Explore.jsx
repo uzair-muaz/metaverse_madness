94122299ad623522d25b8615ed4143cd724d30f9
uzair-muaz <uzair.muaz3k@gmail.com>
2023-09-12 02:00:29 +0500

    Explore section completed

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,42 +1,36 @@
-"use client";
-import { motion } from "framer-motion";
-import { ExploreCard, TypingText, TitleText } from "../components";
+'use client';
 
-import styles from "../styles";
-import { staggerContainer } from "../utils/motion";
-import { exploreWorlds } from "../constants";
-import { useState } from "react";
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { exploreWorlds } from '../constants';
+import { staggerContainer } from '../utils/motion';
+import { ExploreCard, TitleText, TypingText } from '../components';
 
 const Explore = () => {
-  const [active, setActive] = useState("world-2");
+  const [active, setActive] = useState('world-2');
+
   return (
     <section className={`${styles.paddings}`} id="explore">
       <motion.div
         variants={staggerContainer}
-        initial={"hidden"}
-        whileInView={"show"}
+        initial="hidden"
+        whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-        <TypingText title={"| The Worlds"} textStyles="text-center" />
-
+        <TypingText title="| The World" textStyles="text-center" />
         <TitleText
-          title={
-            <>
-              Choose the world you want
-              <br className="md:block hidden" />
-              to explore
-            </>
-          }
+          title={<>Choose the world you want <br className="md:block hidden" /> to explore</>}
           textStyles="text-center"
         />
-
         <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
-          {exploreWorlds.map((world, idx) => (
+          {exploreWorlds.map((world, index) => (
             <ExploreCard
               key={world.id}
               {...world}
-              index={idx}
+              index={index}
               active={active}
               handleClick={setActive}
             />
